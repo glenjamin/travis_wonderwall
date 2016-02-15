0d86b86b0286fdd4d604c4ffa722ed768ff1bb9b
Glen Mailer <glenjamin@gmail.com>
2016-02-15 22:35:58 +0000

    fix tests

--- test.sh
+++ test.sh
@@ -23,39 +23,39 @@ test "All matching"
 echo $should_pass | \
     TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
     TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=master \
-    python travis_wonderwall.py branch=master job=1 version=node
+    ./travis_wonderwall branch=master job=1 version=node
 [ "$?" -eq 0 ] || fail
 
 test "Branch doesn't match"
 echo $should_skip | \
     TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.2 \
     TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=XXX \
-    python travis_wonderwall.py branch=master job=2 version=node
+    ./travis_wonderwall branch=master job=2 version=node
 [ "$?" -eq 0 ] || fail
 
 test "Job doesn't match"
 echo $should_skip | \
     TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
     TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=master \
-    python travis_wonderwall.py branch=master job=2 version=node
+    ./travis_wonderwall branch=master job=2 version=node
 [ "$?" -eq 0 ] || fail
 
 test "Version doesn't match"
 echo $should_skip | \
     TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
     TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=master \
-    python travis_wonderwall.py branch=master version=123
+    ./travis_wonderwall branch=master version=123
 [ "$?" -eq 0 ] || fail
 
 test "Script already failed"
 echo $should_skip | \
     TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
     TRAVIS_TEST_RESULT=1 TRAVIS_BRANCH=master \
-    python travis_wonderwall.py branch=master job=1 version=node
+    ./travis_wonderwall branch=master job=1 version=node
 [ "$?" -eq 0 ] || fail
 
 test "Version doesn't match but job does"
 echo $should_skip | \
     TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 TRAVIS_BRANCH=master \
-    python travis_wonderwall.py branch=master job=1 version=123
+    ./travis_wonderwall branch=master job=1 version=123
 [ "$?" -eq 1 ] || fail
