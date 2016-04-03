48941aac58980f64b6d00f87a93dd2137e45b819
Glen Mailer <glenjamin@gmail.com>
2016-04-03 15:34:45 +0100

    Bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='travis_wonderwall',
-    version='0.1.2',
+    version='0.2.0',
     description='Because after all... you want to run a script on travis',
     author='Glen Mailer',
     author_email='glen@stainlessed.co.uk',
