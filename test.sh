53ca192e48e8f5e9450b4140fbaacc4c9325deba
Glen Mailer <glenjamin@gmail.com>
2016-05-29 18:35:24 +0100

    Add assertion for pull requests

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ fail() {
 
 should_pass='
     echo "Should see this";
-    true
+    exit 42
 '
 
 should_skip='
@@ -19,6 +19,7 @@ should_skip='
 unset TRAVIS_TAG
 unset TRAVIS_PYTHON_VERSION
 unset TRAVIS_BUILD_ID
+unset TRAVIS_PULL_REQUEST
 
 export TRAVIS_TAG=''
 
@@ -27,14 +28,28 @@ echo $should_pass | \
     TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
     TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=master \
     ./travis_wonderwall branch=master job=1 version=node
-[ "$?" -eq 0 ] || fail
+[ "$?" -eq 42 ] || fail
 
 test "All matching with tag"
 echo $should_pass | \
     TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
     TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=v0.1.2 TRAVIS_TAG=v0.1.2\
-    ./travis_wonderwall branch=master job=1 version=node tag=yes
-[ "$?" -eq 0 ] || fail
+    ./travis_wonderwall job=1 version=node tag=yes
+[ "$?" -eq 42 ] || fail
+
+test "All matching on non pull request"
+echo $should_pass | \
+    TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
+    TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=master TRAVIS_PULL_REQUEST=false \
+    ./travis_wonderwall branch=master job=1 version=node pr=false
+[ "$?" -eq 42 ] || fail
+
+test "All matching on pull request"
+echo $should_pass | \
+    TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
+    TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=master TRAVIS_PULL_REQUEST=123 \
+    ./travis_wonderwall branch=master job=1 version=node pr=true
+[ "$?" -eq 42 ] || fail
 
 test "Branch doesn't match"
 echo $should_skip | \
@@ -69,6 +84,20 @@ echo $should_skip | \
     TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 TRAVIS_BRANCH=master \
     ./travis_wonderwall branch=master job=1 version=123
 [ "$?" -eq 1 ] || fail
+
+test "Can skip pull requests"
+echo $should_skip | \
+    TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
+    TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=master TRAVIS_PULL_REQUEST=123 \
+    ./travis_wonderwall branch=master job=1 version=node pr=false
+[ "$?" -eq 0 ] || fail
+
+test "Can skip non pull requests"
+echo $should_skip | \
+    TRAVIS_NODE_VERSION=node TRAVIS_JOB_NUMBER=1.1 \
+    TRAVIS_TEST_RESULT=0 TRAVIS_BRANCH=master TRAVIS_PULL_REQUEST=false \
+    ./travis_wonderwall branch=master job=1 version=node pr=true
+[ "$?" -eq 0 ] || fail
 
 test "Tag wanted but not found "
 echo $should_skip | \
