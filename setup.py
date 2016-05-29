225b5774c78510ab318579a9d394bfeaeef65cdb
Glen Mailer <glenjamin@gmail.com>
2016-05-29 18:37:07 +0100

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='travis_wonderwall',
-    version='0.2.1',
+    version='0.3.0',
     description='Because after all... you want to run a script on travis',
     author='Glen Mailer',
     author_email='glen@stainlessed.co.uk',
